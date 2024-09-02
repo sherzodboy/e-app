9bc82bc968c0cc332aa2ef3d969abfebbbd8f01c
sherzodboy <kholmamatovsh09@gmail.com>
2024-09-02 19:51:29 +0500

    Place Order Payment Methods

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,5 +1,140 @@
+import Title from "../components/Title";
+import CartTotal from "../components/CartTotal";
+import { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+
 const PlaceOrder = () => {
-  return <div>PlaceOrder</div>;
+  const [method, setMethod] = useState("cod");
+  const { navigate } = useContext(ShopContext);
+
+  return (
+    <div className="flex flex-col sm:flex-row justify-around gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* Left Side */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"Delivery"} text2={"Information"} />
+        </div>
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Last Name"
+          />
+        </div>
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="email"
+          placeholder="Email address"
+        />
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="text"
+          placeholder="Street"
+        />
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="City"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="State"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="number"
+            placeholder="Zipcode"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Country"
+          />
+        </div>
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="number"
+          placeholder="Phone"
+        />
+      </div>
+      {/* Right Side */}
+      <div className="mt-8">
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+        <div className="mt-12">
+          <Title text1={"Payment"} text2={"Method"} />
+          {/* Payment method selection */}
+          <div className="flex gap-3 flex-col lg:flex-row">
+            <div
+              onClick={() => setMethod("payme")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "payme" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <img
+                className="h-5 mx-4"
+                src={
+                  "https://www.fibernet.uz/wp-content/uploads/payme-logo.png"
+                }
+                alt="payme-logo"
+              />
+            </div>
+            <div
+              onClick={() => setMethod("visa")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "visa" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <img
+                className="h-5 mx-4"
+                src={
+                  "https://getsby.com/wp-content/uploads/2023/01/Visa-Mastercard-1-1024x378.png"
+                }
+                alt="visa-logo"
+              />
+            </div>
+            <div
+              onClick={() => setMethod("cod")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "cod" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <p className="text-gray-500 text-sm font-medium mx-4">
+                Cash on Deliver
+              </p>
+            </div>
+          </div>
+          <div className="w-full text-end mt-8">
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-black text-white px-16 py-3 text-sm"
+            >
+              Place Order
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceOrder;
