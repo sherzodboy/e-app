5ee621689325b8af77ddc9ecb78e3e0bf4dacb2c
sherzodboy <kholmamatovsh09@gmail.com>
2024-09-03 04:20:14 +0500

    Navbar and Footer logo changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
-import { assets } from "../assets/frontend_assets/assets";
-
 const Footer = () => {
   return (
     <div>
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-20 text-sm">
         <div>
-          <img src={assets.logo} className="mb-3 w-32 " alt="logo" />
+          <h1 className="text-base mb-3">E-Shopping</h1>
           <p className="w-full md:w-2/3 text-gray-600">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
             illo neque inventore aut ducimus cumque, accusantium dolorum quas
